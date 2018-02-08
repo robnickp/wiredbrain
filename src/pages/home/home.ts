49c29d7b6dbac06da1865a53185f3e0b3f1ceaf4
Bobby Pantall <rpantall@ccpace.com>
2018-02-07 20:00:56 -0500

    test visual change

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,7 +12,7 @@ import { FCM } from '@ionic-native/fcm'
 export class HomePage {
 
   menuData = [
-    {title: 'Our Menu', pic:'assets/imgs/soup1.jpg', pushPage: 'MenuPage'},
+    {title: 'Our Menu hh', pic:'assets/imgs/soup1.jpg', pushPage: 'MenuPage'},
     {title: 'Account', pic:'assets/imgs/coffee-people3.jpg', pushPage: 'AccountPage'},
     {title: 'About Us', pic:'assets/imgs/coffee6.jpg', pushPage: 'AboutPage'},
     {title: 'Locations', pic:'assets/imgs/cafe2.jpg', pushPage: 'LocationsPage'}
