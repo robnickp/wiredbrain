517fa1a4b6b764bdf6a889866faf338063fd4af3
Bobby Pantall <rpantall@ccpace.com>
2018-02-15 07:08:50 -0500

    ui fixes

--- src/providers/menu-service/menu-service.ts
+++ src/providers/menu-service/menu-service.ts
@@ -12,19 +12,19 @@ import Promise from 'promise-polyfill'
 export class MenuServiceProvider {
 
   cafe: any[] = [
-    {id:'cof', name: 'Coffee', Description: 'The classic standard, our exclusive select blend',
+    {id:'cof', name: 'Coffee', description: 'The classic standard, our exclusive select blend',
     img: 'assets/imgs/coffee.jpg', small: 1.50, medium: 2.50, large: 3.25},
-    {id:'lat', name: 'Latte', Description: 'The classic latte, our exclusive select blend',
+    {id:'lat', name: 'Latte', description: 'The classic latte, our exclusive select blend',
     img: 'assets/imgs/latte.jpg', small: 2.50, medium: 1.25, large: 3.25},
-    {id:'moc', name: 'Mocha', Description: 'The classic mocha, our exclusive select blend',
+    {id:'moc', name: 'Mocha', description: 'The classic mocha, our exclusive select blend',
     img: 'assets/imgs/mocha.jpg', small: 3.50, medium: 3.13, large: 3.25},
-    {id:'esp', name: 'Espresso', Description: 'The classic espresso, our exclusive select blend',
+    {id:'esp', name: 'Espresso', description: 'The classic espresso, our exclusive select blend',
     img: 'assets/imgs/espresso.jpg', small: 5.50, medium: 3.47, large: 3.25},
-    {id:'cap', name: 'Cappuccino', Description: 'The classic cappuccino, our exclusive select blend',
+    {id:'cap', name: 'Cappuccino', description: 'The classic cappuccino, our exclusive select blend',
     img: 'assets/imgs/cappuccino.jpg', small: 2.22, medium: 3.33, large: 3.25},
-    {id:'ame', name: 'Americano', Description: 'The classic americano, our exclusive select blend',
+    {id:'ame', name: 'Americano', description: 'The classic americano, our exclusive select blend',
     img: 'assets/imgs/coffee.jpg', small: 4.44, medium: 3.50, large: 3.25},
-    {id:'mac', name: 'Macchiato', Description: 'The classic macchiato, our exclusive select blend',
+    {id:'mac', name: 'Macchiato', description: 'The classic macchiato, our exclusive select blend',
     img: 'assets/imgs/macchiato.jpg', small: 7.66, medium: 4.50, large: 3.25}
   ]
 
